8cfbb714dab409513b56a48f738eb3a34c04ed45
Holytis <holytis123@gmail.com>
2022-12-15 17:47:46 +0700

    sed laew

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int x , o = 0 , e = 0  ;
+    
+    while (x!=0)
+    {
+        cout << "Enter an integer: ";
+        cin >> x ;
+        if (x!=0)
+        {
+             if (x%2 == 0)
+                {
+                    e++ ;
+                }
+                else
+                    {
+                        o++ ;
+                    }
+        
+        }
+    }
+        cout << "#Even numbers = " << e << endl ;
+        cout << "#Odd numbers = " << o << endl ;
+
     return 0;
+
 }
